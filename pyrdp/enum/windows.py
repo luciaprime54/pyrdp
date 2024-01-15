860c64f6f3131c679cbb8cf482db9841c3303358
luciaprime54 <156635296+luciaprime54@users.noreply.github.com>
2024-01-15 05:17:53 +0000

    Handle missing Windows error codes thrown by some RDP clients

--- pyrdp/enum/windows.py
+++ pyrdp/enum/windows.py
@@ -28,6 +28,21 @@ class NTSTATUS(IntEnum):
     [MS-ERREF]: Windows Error Codes
     https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-erref/87fba13e-bf06-450e-83b1-9241dc81e781
     """
+
+    # Various RDP clients can return other error codes.
+    # Instead of listing every Windows Error code,
+    # we just list the ones we care about, and create a 'catch-all' value
+    @classmethod
+    def _missing_(cls, value):
+        """
+        Handle a missing Windows error code by dynamically creating it
+        :param value: The error code
+        """
+        other = NTSTATUS(0x00000000)
+        other._name_ = "STATUS_PYRDP_FAILURE"
+        other._value_ = value
+        return other
+
     STATUS_SUCCESS       = 0x00000000
     STATUS_NO_MORE_FILES = 0x80000006
     STATUS_NO_SUCH_FILE  = 0xC000000F
